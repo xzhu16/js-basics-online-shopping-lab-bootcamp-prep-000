10a7e28d70cbc6f2446609aa30a5be3aff83157b
Xi Zhu <zxjucy2012@gmail.com>
2019-05-05 16:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,41 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var price = Math.ceil(Math.random()*100)
+  cart.push({[item]:price})
+  console.log(`${item} has been added to your cart·`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  }
+  var newarr = [];
+    for (var i = 0; i < cart.length; i++) {
+      var ks = Object.keys(cart[i])[0]
+      newarr.push(ks + "at $" + cart[i][ks])
+    }
+    var text = "In your cart, you have "
+      if (newarr.length === 1) {
+        text += newarr + "."
+      } else if (newarr.length === 2) {
+        text += (newarr[0] + " and " + newarr[1] + ".")
+      } else if (newarr.length > 2) {
+        
+      }
+  
+  
+  
+  
 }
+  
+  
+
 
 function total() {
   // write your code here
+  
 }
 
 function removeFromCart(item) {
