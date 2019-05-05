03a40c0aaa40cb61f12e3e2c4bcf1e250c538d56
Xi Zhu <zxjucy2012@gmail.com>
2019-05-05 17:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-  var price = Math.ceil(Math.random()*100)
+  var price = Math.floor(Math.random()*100)
   cart.push({[item]:price})
   console.log(`${item} has been added to your cart·`)
   return cart
